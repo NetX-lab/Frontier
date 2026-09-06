f90d6427dec9057abeb9e7de16783e2ed9e6a4bd
YICHENGFENG <50061432+fwyc0573@users.noreply.github.com>
2026-09-06 13:34:06 +0800

    refactor: extract metrics execution-time builder

--- frontier/scheduler/cluster_scheduler/base_cluster_scheduler.py
+++ frontier/scheduler/cluster_scheduler/base_cluster_scheduler.py
@@ -88,6 +88,9 @@ from frontier.scheduler.utils.collective_timing import (
     select_active_batch,
     validate_decode_layer_advance,
 )
+from frontier.scheduler.utils.execution_time_metrics import (
+    build_single_layer_metrics_execution_time,
+)
 from frontier.scheduler.replica_stage_scheduler.stage_execution_context import (
     EP_WAVE,
     FULL_STAGE_WORLD,
@@ -4073,65 +4076,8 @@ class BaseClusterScheduler(ABC):
         original_start_time,
     ):
         """Create corrected ExecutionTime payload used by metrics/trace emission."""
-        from frontier.entities.execution_time import ExecutionTime
-
-        corrected_execution_time = ExecutionTime(
-            num_layers_per_pipeline_stage=1,  # Avoid double-counting in sync path.
-            attention_rope_execution_time=original_execution_time._attention_rope_execution_time,
-            attention_kv_cache_save_execution_time=original_execution_time._attention_kv_cache_save_execution_time,
-            attention_decode_execution_time=original_execution_time._attention_decode_execution_time,
-            attention_prefill_execution_time=original_execution_time._attention_prefill_execution_time,
-            attention_layer_pre_proj_execution_time=original_execution_time._attention_layer_pre_proj_execution_time,
-            attention_layer_post_proj_execution_time=original_execution_time._attention_layer_post_proj_execution_time,
-            attn_norm_time=original_execution_time._attn_norm_time,
-            mlp_norm_time=original_execution_time._mlp_norm_time,
-            add_time=original_execution_time._add_time,
-            add_attn_residual_time=original_execution_time._add_attn_residual_time,
-            add_ffn_residual_time=original_execution_time._add_ffn_residual_time,
-            tensor_parallel_communication_time=original_execution_time._tensor_parallel_communication_time,
-            attn_tensor_parallel_allreduce_time=(
-                original_execution_time._attn_tensor_parallel_allreduce_time
-                if original_execution_time._has_attn_tensor_parallel_allreduce_time
-                else None
-            ),
-            moe_tensor_parallel_allreduce_time=(
-                original_execution_time._moe_tensor_parallel_allreduce_time
-                if original_execution_time._has_moe_tensor_parallel_allreduce_time
-                else None
-            ),
-            tensor_parallel_allgather_time=original_execution_time._tensor_parallel_allgather_time,
-            share_expert_tensor_parallel_allreduce_time=original_execution_time._share_expert_tensor_parallel_allreduce_time,
-            dp_input_allreduce_time=original_execution_time._dp_input_allreduce_time,
-            dp_output_allreduce_time=original_execution_time._dp_output_allreduce_time,
-            pipeline_parallel_communication_time=original_execution_time._pipeline_parallel_communication_time,
-            expert_parallel_communication_time=original_execution_time._expert_parallel_communication_time,
-            moe_gating_time=original_execution_time._moe_gating_time,
-            moe_gating_linear_time=original_execution_time._moe_gating_linear_time,
-            moe_gating_routing_topk_time=original_execution_time._moe_gating_routing_topk_time,
-            moe_shuffling_time=original_execution_time._moe_shuffling_time,
-            schedule_time=original_execution_time._schedule_time,
-            sampler_e2e_time=original_execution_time._sampler_e2e_time,
-            prepare_inputs_e2e_time=original_execution_time._prepare_inputs_e2e_time,
-            pp_producer_send_path_runtime_time=original_execution_time._pp_producer_send_path_runtime_time,
-            pp_receiver_head_runtime_time=original_execution_time._pp_receiver_head_runtime_time,
-            pp_prefill_consumer_active_runtime_time=original_execution_time._pp_prefill_consumer_active_runtime_time,
-            process_model_outputs_time=original_execution_time._process_model_outputs_time,
-            ray_comm_time=original_execution_time._ray_comm_time,
-            is_moe=original_execution_time._is_moe,
-            mlp_layer_up_proj_execution_time=original_execution_time._mlp_layer_up_proj_execution_time,
-            mlp_layer_down_proj_execution_time=original_execution_time._mlp_layer_down_proj_execution_time,
-            mlp_layer_act_execution_time=original_execution_time._mlp_layer_act_execution_time,
-            moe_grouped_gemm_time=original_execution_time._moe_grouped_gemm_time,
-            share_expert_up_proj_time=original_execution_time._share_expert_up_proj_time,
-            share_expert_down_proj_time=original_execution_time._share_expert_down_proj_time,
-            share_expert_act_time=original_execution_time._share_expert_act_time,
-            decode_draft_proposer_time=original_execution_time._decode_draft_proposer_time,
-            mtp_terminal_overshoot_time=(
-                original_execution_time._mtp_terminal_overshoot_time
-            ),
-        )
-
-        return corrected_execution_time
+        del actual_execution_time_ms, original_start_time
+        return build_single_layer_metrics_execution_time(original_execution_time)
 
     def _record_mtp_terminal_completion_delay(
         self,
